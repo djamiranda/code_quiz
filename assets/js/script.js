97bdca10025d6bcbd412fa4685afc085a4803900
djamiranda <djamiranda@gmail.com>
2023-05-11 13:39:09 -0400

    added question, score & timer arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,4 +26,14 @@ var questions = [
     },
   ];
 
-  
+var score = 0;
+var questionList = 0;
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startQuiz");
+var questionsSection = document.querySelector("#questionsSection");
+var wrapper = document.querySelector("#wrapper");
+
+var secondsLeft = 60;
+var holdInterval = 0;
+var penalty = 10;
+var ulCreate = document.createElement("ul");
